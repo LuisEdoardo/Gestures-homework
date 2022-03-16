aea8c8e30a21a1b884fc7d16c01adeafb340df09
Luis Leyva <lmorales6@ucol.mx>
2022-03-16 00:14:28 -0600

    Add project files.

---
+++ Gestures-homework/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Gestures_homework
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        async void OnTapGestureRecognizerTapped(object sender, EventArgs args)
+        {
+            var imagen = (Image)sender;
+           await this.Navigation.PushModalAsync(new ModalPage(imagen));
+        }
+    }
+}
---
+++ Gestures-homework/ModalPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Gestures_homework
+{
+public partial class ModalPage : ContentPage
+{
+    public ModalPage(object sender)
+    {
+        InitializeComponent();
+            var image = (Image)sender;
+            img.Source = image.Source;
+    }
+        void OnPinched(object sender, PinchGestureUpdatedEventArgs e)
+        {
+            this.Navigation.PopModalAsync();
+        }
+
+    }
+}
